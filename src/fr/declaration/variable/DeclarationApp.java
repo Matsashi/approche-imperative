2820f8f96a915c11fb628e929edf33bf039c1d81
Matsashi <matthew.pottier@hotmail.fr>
2022-10-06 16:42:12 +0200

    tp3 done

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,24 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 6;
+		short b = 300;
+		int c = 33000;
+		long d = 120_000;
+		float e = 1.5f;
+		double f = 10.85;
+		char g = 'g';
+		String h = "Hâche";
+		boolean i = true;
+		
+		System.out.println(h);
+		
+		String randomString = "Voici le résultat d'un calcul : 1+5=6";
+		System.out.println(randomString);
+		
+	}
+
+}
